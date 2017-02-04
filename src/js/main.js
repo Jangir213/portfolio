c16f3e8940ac399450b98958e3449fb85b24bd99
jangir <jangirhus@gmail.com>
2017-02-04 23:14:12 +0200

    add libs

---
+++ src/js/main.js
@@ -0,0 +1,65 @@
+(function ($) {
+
+  $(window).on('load', function () {
+    navChanged();
+    scrollSmooth();
+    particlesJS.load('promo', './js/particleconfig.json');
+    typewriter.type(function () {
+      typewriter2.type();
+    });
+  });
+
+  function navChanged() {
+    var $navLinks = $('.nav__link');
+
+    $navLinks.each(function () {
+      var $this = $(this);
+      var id = $this.attr('href');
+      var $section = $(id);
+
+      $section.scrollex({
+        mode: 'middle',
+        top: '10vh',
+        bottom: '-10vh',
+
+        enter: function () {
+          $this.addClass('active');
+        },
+
+        leave: function () {
+          $this.removeClass('active');
+        }
+      });
+
+    });
+
+  }
+
+  function scrollSmooth() {
+    $('.nav__items').on('click', 'a', click);
+    $('.promo__btn').on('click', click);
+
+    function click(event) {
+      var id = $(this).attr('href');
+      var top = $(id).offset().top;
+
+      event.preventDefault();
+      $('body,html').animate({ scrollTop: top }, 1500);
+    }
+  }
+
+  var typewriter = new Typewriter('.promo__name', {
+    text: 'Jahangir Hussain',
+    interval: 100
+  });
+
+  var typewriter2 = new Typewriter('.promo__description', {
+    text: 'Front-end developer',
+    interval: 100
+  });
+
+
+
+
+
+})(jQuery);
